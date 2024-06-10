97f89b21b48b8cad6fe19c66f4a26ebff1a505e1
AG6Dev <ag6_pm@protonmail.com>
2024-06-11 00:24:19 +0100

    redirect back toh ome

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,10 @@
 import {auth} from "@/auth";
 import SignOutButton from "@/app/components/SignOutButton";
+import {redirect} from "next/navigation";
 
 export default async function Dashboard() {
     const session = await auth();
+    if(!session) redirect("/")
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
